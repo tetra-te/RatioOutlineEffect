d31e16bbbd5e9d32bd7c3091f3c876220d504649
tetra_te <node4853@gmail.com>
2026-03-04 09:15:27 +0900

    エフェクト使用時に例外が発生する問題を修正

--- RatioOutlineEffect/Init.cs
+++ RatioOutlineEffect/Init.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using System.Windows;
 using YukkuriMovieMaker.Commons;
 using YukkuriMovieMaker.Plugin;
@@ -20,5 +21,25 @@ namespace RatioOutlineEffect
                 MessageBox.Show($"以下のファイルはこのバージョンでは不要ですので削除をおすすめします。\r\n\r\n{harmonyPath.Replace('\\', '/')}", "比率縁取りプラグイン");
             }
         }
+    }
+
+    internal static class Reflection
+    {
+        private static Assembly yukkuriMovieMaker = Assembly.Load("YukkuriMovieMaker");
+
+        public static Type OutlineEffect = yukkuriMovieMaker.GetType("YukkuriMovieMaker.Project.Effects.OutlineLiteEffect") ?? yukkuriMovieMaker.GetType("YukkuriMovieMaker.Project.Effects.OutlineEffect")!;
+
+        public static PropertyInfo? StrokeThickness = OutlineEffect.GetProperty("StrokeThickness", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Blur = OutlineEffect.GetProperty("Blur", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Quality = OutlineEffect.GetProperty("Quality", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Smoothness = OutlineEffect.GetProperty("Smoothness", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? IsOutlineOnly = OutlineEffect.GetProperty("IsOutlineOnly", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? IsAngular = OutlineEffect.GetProperty("IsAngular", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? X = OutlineEffect.GetProperty("X", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Y = OutlineEffect.GetProperty("Y", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Opacity = OutlineEffect.GetProperty("Opacity", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Zoom = OutlineEffect.GetProperty("Zoom", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? Rotation = OutlineEffect.GetProperty("Rotation", BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo? StrokeBrush = OutlineEffect.GetProperty("StrokeBrush", BindingFlags.Public | BindingFlags.Instance);
     }
 }
--- RatioOutlineEffect/RatioOutlineEffectProcessor.cs
+++ RatioOutlineEffect/RatioOutlineEffectProcessor.cs
@@ -2,8 +2,9 @@
 using Vortice.Direct2D1;
 using YukkuriMovieMaker.Commons;
 using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Brush;
+using YukkuriMovieMaker.Plugin.Effects;
 using YukkuriMovieMaker.Project;
-using YukkuriMovieMaker.Project.Effects;
 using YukkuriMovieMaker.Project.Items;
 
 namespace RatioOutlineEffect
@@ -12,7 +13,7 @@ namespace RatioOutlineEffect
     {
         readonly RatioOutlineEffect param;
         
-        readonly OutlineLiteEffect outlineItem;
+        readonly IVideoEffect outlineItem;
         readonly IVideoEffectProcessor outlineProcessor;
 
         ID2D1Image? input;
@@ -22,9 +23,10 @@ namespace RatioOutlineEffect
 
         public RatioOutlineEffectProcessor(IGraphicsDevicesAndContext devices, RatioOutlineEffect item)
         {
-            param= item;
-            this.devices= devices;
-            outlineItem = new OutlineLiteEffect();
+            param = item;
+            this.devices = devices;
+
+            outlineItem = (IVideoEffect)Activator.CreateInstance(Reflection.OutlineEffect);
             outlineProcessor = outlineItem.CreateVideoEffect(devices);
             Output = outlineProcessor.Output;
         }      
@@ -127,45 +129,82 @@ namespace RatioOutlineEffect
                 }
             }
 
-            var strokeThickness = size * param.StrokeThicknessRate.GetValue(frame, length, fps) / 100;
-            strokeThickness = Math.Min(strokeThickness, 500);
-            SetAnimationValue(outlineItem.StrokeThickness, strokeThickness);
 
-            var blur = size * param.BlurRate.GetValue(frame, length, fps) / 100;
-            SetAnimationValue(outlineItem.Blur, blur);
-
-            var quality = param.Quality.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.Quality, quality);
-
-            var smoothness = param.Smoothness.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.Smoothness, smoothness);
-
-            outlineItem.IsOutlineOnly = param.IsOutlineOnly;
-
-            outlineItem.IsAngular = param.IsAngular;
-
-            var x = param.X.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.X, x);
-
-            var y = param.Y.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.Y, y);
-
-            var opacity = param.Opacity.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.Opacity, opacity);
-
-            var zoom = param.Zoom.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.Zoom, zoom);
-
-            var rotation = param.Rotation.GetValue(frame, length, fps);
-            SetAnimationValue(outlineItem.Rotation, rotation);
-
-            outlineItem.StrokeBrush.Parameter = param.StrokeBruch.Parameter;
+            if (Reflection.StrokeThickness is not null)
+            {
+                var strokeThickness = size * param.StrokeThicknessRate.GetValue(frame, length, fps) / 100;
+                strokeThickness = Math.Min(strokeThickness, 500);
+                var anm = (Animation?)Reflection.StrokeThickness.GetValue(outlineItem);
+                SetAnimationValue(anm, strokeThickness);
+            }
+            if (Reflection.Blur is not null)
+            {
+                var blur = size * param.BlurRate.GetValue(frame, length, fps) / 100;
+                var anm = (Animation?)Reflection.Blur.GetValue(outlineItem);
+                SetAnimationValue(anm, blur);
+            }
+            if (Reflection.Quality is not null)
+            {
+                var quality = param.Quality.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.Quality.GetValue(outlineItem);
+                SetAnimationValue(anm, quality);
+            }
+            if (Reflection.Smoothness is not null)
+            {
+                var smoothness = param.Smoothness.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.Smoothness.GetValue(outlineItem);
+                SetAnimationValue(anm, smoothness);
+            }
+            if (Reflection.IsOutlineOnly is not null)
+            {
+                Reflection.IsOutlineOnly.SetValue(outlineItem, param.IsOutlineOnly);
+            }
+            if (Reflection.IsAngular is not null)
+            {
+                Reflection.IsAngular.SetValue(outlineItem, param.IsAngular);
+            }
+            if (Reflection.X is not null)
+            {
+                var x = param.X.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.X.GetValue(outlineItem);
+                SetAnimationValue(anm, x);
+            }
+            if (Reflection.Y is not null)
+            {
+                var y = param.Y.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.Y.GetValue(outlineItem);
+                SetAnimationValue(anm, y);
+            }
+            if (Reflection.Opacity is not null)
+            {
+                var opacity = param.Opacity.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.Opacity.GetValue(outlineItem);
+                SetAnimationValue(anm, opacity);
+            }
+            if (Reflection.Zoom is not null)
+            {
+                var zoom = param.Zoom.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.Zoom.GetValue(outlineItem);
+                SetAnimationValue(anm, zoom);
+            }
+            if (Reflection.Rotation is not null)
+            {
+                var rotation = param.Rotation.GetValue(frame, length, fps);
+                var anm = (Animation?)Reflection.Rotation.GetValue(outlineItem);
+                SetAnimationValue(anm, rotation);
+            }
+            if (Reflection.StrokeBrush is not null)
+            {
+                var brush = (Brush?)Reflection.StrokeBrush.GetValue(outlineItem);
+                brush?.Parameter = param.StrokeBruch.Parameter;
+            }
 
             return outlineProcessor.Update(effectDescription);
         }
 
-        static void SetAnimationValue(Animation animation, double value)
+        static void SetAnimationValue(Animation? animation, double value)
         {
+            if (animation is null) return;
             var current = animation.GetValue(0, 1, 30);
             animation.AddToEachValues(value - current);
         }
